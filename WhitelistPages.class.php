257fd70a6156dd74f991075547e34e935d4bf2e5
Jack Phoenix <jack@countervandalism.net>
2017-01-14 15:52:46 +0200

    Version 0.5.0: rewritten to use the TitleReadWhitelist hook
    Old PHP entry point dropped, a modern MW (1.25+) is now required to use
    this extension.
    
    Change-Id: I7e21e9b67f38a4c7ad1899cbbc80d7aed1bd292e

--- WhitelistPages.class.php
+++ WhitelistPages.class.php
@@ -6,38 +6,40 @@
  * @ingroup Extensions
  * @author Jack Phoenix <jack@shoutwiki.com>
  * @author Misza <misza@shoutwiki.com>
- * @date 19 November 2015
+ * @date 21 December 2016
  * @license https://en.wikipedia.org/wiki/Public_domain Public domain
  */
 
 class WhitelistPages {
 
-	public static function main() {
-		global $wgWhitelistRead, $wgGroupPermissions;
+	/**
+	 * @param Title $title Title object being checked against
+	 * @param User $user Current user object
+	 * @param bool &$whitelisted Whether this title is whitelisted
+	 */
+	public static function onTitleReadWhitelist( $title, $user, &$whitelisted ) {
+		global $wgGroupPermissions;
 
 		$message = wfMessage( 'public_read_whitelist' )->inContentLanguage();
 
-		// If MediaWiki:Public read whitelist is empty, bail out
+		// If [[MediaWiki:Public read whitelist]] is empty, bail out
 		if ( $message->isDisabled() ) {
-			return;
+			return true;
 		}
 
 		// If anonymous users can read the wiki, then it's not a private one
 		// and we don't need this feature for non-private wikis
 		if ( $wgGroupPermissions['*']['read'] ) {
-			return;
-		}
-
-		// $wgWhitelistRead is *false* by default instead of being an empty array
-		if ( $wgWhitelistRead === false ) {
-			$wgWhitelistRead = array();
+			return true;
 		}
 
 		// Explode along newlines
 		$whitelistedPages = explode( "\n", trim( $message->plain() ) );
+		if ( in_array( $title->getPrefixedText(), $whitelistedPages ) ) {
+			$whitelisted = true;
+		}
 
-		// Merge with current list
-		$wgWhitelistRead = array_merge( $wgWhitelistRead, $whitelistedPages );
+		return true;
 	}
 
 }
