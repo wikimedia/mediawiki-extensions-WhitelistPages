f46f68e2da2590fe919b1f86ee9ceda3ad20d4fd
thiemowmde <thiemo.kreuz@wikimedia.de>
2025-11-10 12:07:38 +0100

    Update code to work with recent MediaWiki versions
    * Use backported Title namespace.
    * Remove meaningless return true from hook code.
    * Remove meaningless PHPDoc tags.
    
    Change-Id: Idd33e7f1ab2fbd17b6333133cf4fbb9d1de5e924

--- WhitelistPages.php
+++ WhitelistPages.php
@@ -1,22 +1,21 @@
 <?php
+
+use MediaWiki\Title\Title;
+
 /**
  * A quick hack to make $wgWhitelistRead an admin-editable system message.
  *
- * @file
- * @ingroup Extensions
  * @author Jack Phoenix <jack@shoutwiki.com>
  * @author Misza <misza@shoutwiki.com>
- * @date 13 July 2017
  * @license https://en.wikipedia.org/wiki/Public_domain Public domain
  */
-
 class WhitelistPages {
 
 	/**
 	 * @param Title $title Title object being checked against
 	 * @param User $user Current user object
 	 * @param bool &$whitelisted Whether this title is whitelisted
-	 * @return true
+	 * @return void
 	 */
 	public static function onTitleReadWhitelist( $title, $user, &$whitelisted ) {
 		global $wgGroupPermissions;
@@ -25,13 +24,13 @@ class WhitelistPages {
 
 		// If [[MediaWiki:Public read whitelist]] is empty, bail out
 		if ( $message->isDisabled() ) {
-			return true;
+			return;
 		}
 
 		// If anonymous users can read the wiki, then it's not a private one
 		// and we don't need this feature for non-private wikis
 		if ( $wgGroupPermissions['*']['read'] ) {
-			return true;
+			return;
 		}
 
 		static $whitelistedPages;
@@ -42,8 +41,6 @@ class WhitelistPages {
 		if ( in_array( $title->getPrefixedText(), $whitelistedPages ) ) {
 			$whitelisted = true;
 		}
-
-		return true;
 	}
 
 }
